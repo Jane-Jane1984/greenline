368296699047bd867dff216ccd55b9d4f9ce06e1
Volkova <study@study.com>
2020-10-12 21:40:09 +0300

    add function renderTaplaem

---
+++ core/init.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/config.php'; //подключаем настройки
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/db.php'; //Подключаем соединение с базой
+require_once $_SERVER['DOCUMENT_ROOT'] . '/core/function.php'; // Подключение функций
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'core/init.php';
+
+$title = 'Главная страница';
+
+$res = mysqli_query($link, "SELECT * FROM `category` ORDER BY `title` ASC");
+$arCategory = mysqli_fetch_all($res, MYSQLI_ASSOC);
+
+// echo '<pre>';
+// print_r($arCategory);
+// echo '</pre>';
+
+
+$page_content = renderTemplate("main");
+
+
+$res = renderTemplate('layout', [
+                        'content' => $page_content, 
+                        'title' => 'Главная страница',
+                        'arCategory' => $arCategory
+                        ]);
+
+echo $res;
+
+?>
